b7559bf059419bf1a577dd60ad446809f3dce6c6
Drun1baby <2717763591@qq.com>
2023-03-27 19:51:13 +0800

    :fix:删除信息泄露

---
+++ JavaSecurity/Shiro/shiro682/src/main/java/shiro/pojo/User.java
@@ -0,0 +1,15 @@
+package shiro.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private int id;
+    private String name;
+    private String pwd;
+    private String perms;
+}
